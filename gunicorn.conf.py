e9b6e8318e1a4c72b4d52e57d59eb68be77dc4c0
Gomonchuk <vladgomonchuk@gmail.com>
2025-06-09 11:33:42 +0300

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-bind = 'unix:/run/gunicorn.sock'
-workers = 3
-errorlog = '/home/ubuntu/Desktop/Projects/app/error.log'
-accesslog = '/home/ubuntu/Desktop/Projects/app/access.log'
-loglevel = 'info'
-daemon = False
